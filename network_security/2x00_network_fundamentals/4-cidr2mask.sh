2fcf420b115689a230c587795451c30c3835e537
OmarAlizada <omer.alizade57@gmail.com>
2026-05-18 22:19:54 +0000

    Fix 4-cidr2mask.sh pattern matching for 255 and edge cases

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bits=$(printf "%032d" 0 | sed "s/0/1/g" | cut -c1-"$1")$(printf "%032d" 0 | cut -c1-$((32 - $1))); printf "%d.%d.%d.%d\n" "$((2#${bits:0:8}))" "$((2#${bits:8:8}))" "$((2#${bits:16:8}))" "$((2#${bits:24:8}))"
+vals=(255 255 255 255 0); mask=$((0xffffffff << (32 - $1))); [ "$1" -eq 0 ] && echo "0.0.0.0" || printf "%d.%d.%d.%d\n" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
