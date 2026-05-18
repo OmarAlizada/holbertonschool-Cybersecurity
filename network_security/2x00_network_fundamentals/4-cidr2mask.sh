1376732da2e3da5467285c4da5b3435e28801070
OmarAlizada <omer.alizade57@gmail.com>
2026-05-18 22:17:48 +0000

    Fix 4-cidr2mask.sh trailing character issue

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$((0xffffffff ^ (1 << (32 - $1)) - 1)); echo "$(( (mask >> 24) & 255 )).$(( (mask >> 16) & 255 )).$(( (mask >> 8) & 255 )).$(( mask & 255 ))"
+mask=$((0xffffffff ^ (1 << (32 - $1)) - 1)); echo "$(( (mask >> 24) & 255 )).$(( (mask >> 16) & 255 )).$(( (mask >> 8) & 255 )).$(( mask & 255 ))" | tr -d "[:space:]" | sed "s/$/\n/"
